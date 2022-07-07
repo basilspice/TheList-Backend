75e3a14871d2b5c42cf39c6aaf682f061e986d21
basil <bizzlebreton@gmail.com>
2022-07-07 12:14:30 -0700

    updated settings to include psql and api

--- the_list/settings.py
+++ the_list/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Our Apps
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'the_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'thelist',
+        'USER': 'postgres',
+        'PASSWORD': 'sei'
+        
     }
 }
 
