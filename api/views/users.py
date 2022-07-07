e7d1eb2c802b134cc1ddef9a91fd3112f59b6eea
basil <bizzlebreton@gmail.com>
2022-07-07 13:56:28 -0700

    sign-in added not working

--- api/views/users.py
+++ api/views/users.py
@@ -1,24 +1,23 @@
-
 from rest_framework.response import Response
 from rest_framework import status, generics
 from rest_framework.authtoken.models import Token
 from ..serializers.user import UserSerializer
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 class SignUp(generics.CreateAPIView):
-        # Override the authentication/permissions classes so this endpoint
-        # is not authenticated & we don't need any permissions to access it.
-        authentication_classes = ()
-        permission_classes = ()
+    # Override the authentication/permissions classes so this endpoint
+    # is not authenticated & we don't need any permissions to access it.
+    authentication_classes = ()
+    permission_classes = ()
 
-        def post(self, request):
-            new_user = UserSerializer(data=request.data)
-            if new_user.is_valid():
-                  # Save the user and send back a response!
-                new_user.save()
-                return Response({ 'user': new_user.data }, status=status.HTTP_201_CREATED)
-            else:
-                return Response(new_user.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        new_user = UserSerializer(data=request.data)
+        if new_user.is_valid():
+                # Save the user and send back a response!
+            new_user.save()
+            return Response({ 'user': new_user.data }, status=status.HTTP_201_CREATED)
+        else:
+            return Response(new_user.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class SignIn(generics.CreateAPIView):
     # Override the authentication/permissions classes so this endpoint
@@ -49,3 +48,14 @@ class SignIn(generics.CreateAPIView):
             })
         else:
             return Response({ 'msg': 'The username and/or password is incorrect.' }, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+class SignOut(generics.DestroyAPIView):
+    def delete(self, request):
+        user = request.user
+        # Remove this token from the user
+        Token.objects.filter(user=user).delete()
+        user.token = None
+        user.save()
+        # Logout will remove all session data
+        logout(request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
